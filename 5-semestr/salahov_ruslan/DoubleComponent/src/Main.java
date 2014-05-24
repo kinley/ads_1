85a3a5b91d287f69ef9ffabffc39efad51c6b1d4
Ridel1e <ridellee@gameil.com>
2014-05-24 20:33:47 +0400

    Sem1

--- 5-semestr/salahov_ruslan/DoubleComponent/src/Main.java
+++ 5-semestr/salahov_ruslan/DoubleComponent/src/Main.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 public class Main {
 	 public static void main(String[] args) throws IOException {
 
-		 // Пример работы на среднем графе.
+		 // В комплекте 4 файла для тестов: обычный граф, граф с большим числом вершин и ребер, файл с нулем и граф без ребер.
+		 // cоответственно input.txt, inputNoVex.txt, inputIsolatedVex.txt, inputBigG.txt 
 	        Scanner sc = new Scanner(new File("input.txt"));
 	        PrintWriter pw = new PrintWriter(new File("output.txt"));
 	        TextParser textParser = new TextParser(sc, pw);                  
@@ -18,50 +19,10 @@ public class Main {
 	        сomponent1.FindComponent();
 	      //  
 
-	      // Граф, не имеющий вершин.
-	        Scanner sc1 = new Scanner(new File("inputNoVex.txt"));
-	        PrintWriter pw1 = new PrintWriter(new File("outputNoVex.txt"));
-	        TextParser textParser1 = new TextParser(sc1, pw1);                  
-
-	        Graph G1 = new Graph(textParser1);                                
-	        G1.PrintGraph(textParser1);
-
-	        DoubleComponent сomponent2 = new DoubleComponent(G1,textParser1);
-	        сomponent2.FindComponent();	        
-	      //
-	        
-		  // Граф, имеющий 5 изолированных вершин.
-	        Scanner sc2 = new Scanner(new File("inputIsolatedVex.txt"));
-	        PrintWriter pw2 = new PrintWriter(new File("outputIsolatedVex.txt"));
-	        TextParser textParser2 = new TextParser(sc2, pw2);                  
-
-	        Graph G2 = new Graph(textParser2);                                
-	        G2.PrintGraph(textParser2);
-
-	        DoubleComponent сomponent3 = new DoubleComponent(G2,textParser2);
-	        сomponent3.FindComponent();	        
-	      //
-	        
-		  // Граф с большим количеством вершин.
-	        Scanner sc3 = new Scanner(new File("inputBigG.txt"));
-	        PrintWriter pw3 = new PrintWriter(new File("outputBigG.txt"));
-	        TextParser textParser3 = new TextParser(sc3, pw3);                  
-
-	        Graph G3 = new Graph(textParser3);                                
-	        G3.PrintGraph(textParser3);
-
-	        DoubleComponent сomponent4 = new DoubleComponent(G3,textParser3);
-	        сomponent4.FindComponent();	        
-	      //  
+	     
 	        
 	        pw.close();
 	        sc.close();
-	        pw1.close();
-	        sc1.close();
-	        pw2.close();
-	        sc2.close();
-	        pw3.close();
-	        sc3.close();
 	    }
 }
 
