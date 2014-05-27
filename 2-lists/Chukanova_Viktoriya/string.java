5c2617714200b58404f051eec9562014a90c1502
VChar <chukanova.vv@gmail.com>
2014-05-28 02:35:48 +0400

    ...

---
+++ 2-lists/Chukanova_Viktoriya/string.java
@@ -0,0 +1,64 @@
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+ 
+class Stack
+{
+    public static void PrintValues(IEnumerable myCollection)
+    {
+    	 System.out.println("    {0}");
+    }
+		public void Main(string[] args)
+		{
+		Scanner in = new Scanner(System.in);
+	    Object qwer2;
+	    Stack stk = new Stack();
+	    string term, qwer1, a1, a2, a3;
+	    System.out.println( "Write term in OPN: ");
+		}
+}
+	public class string
+	{
+	 void main (string s)
+		 {
+        if s " " int "[A-Za-z0-9]" Regx 
+        {
+        	 void foren (char str in s)
+        	{
+                if ( rgx.IsMatch(s) )
+                 stk.Push(s);
+                else
+                {
+                    if ( s == "+" || s == "-" )
+                    {
+                        qwer2 = stk.Pop();
+                        a3 = qwer2.ToString();
+                        a2 = s.ToString();
+                        qwer2 = stk.Pop();
+                        a1 = qwer2.ToString();
+                        qwer1 = string.Concat( "(" + a1 + a2 + a3 + ")" );
+                        stk.Push(qwer1);
+                    }
+                    else
+                    {
+                        qwer2 = stk.Pop();
+                        a3 = qwer2.ToString();
+                        a2 = s.ToString();
+                        qwer2 = stk.Pop();
+                        a1 = qwer2.ToString();
+                        qwer1 = string.Concat( a1 + a2 + a3 );
+                        stk.Push(qwer1);
+                    }
+        
+            System.out.print(stk);
+        }
+            }
+            
+    
+    
+       
+
+
+
+
