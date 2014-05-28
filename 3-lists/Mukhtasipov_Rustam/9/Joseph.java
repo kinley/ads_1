129d66409ef4f788330683e2267bbf77d3760068
Far4Mac <far4mac@gmail.com>
2014-05-28 10:01:48 +0400

    commit

---
+++ 3-lists/Mukhtasipov_Rustam/9/Joseph.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Joseph {
+   public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Введите количество:");
+     int n=sc.nextInt();sc.nextLine();           
+     System.out.println("Введите номер исключаемого:");
+     int m=sc.nextInt();sc.nextLine();           
+     List<Integer> l=new ArrayList<Integer>();
+     for (int i=1;i<=n;i++)
+         l.add(i);                              
+     int ost=n;
+     int h=-1;
+     while(ost>1)
+       {
+         h+=m;
+         if (h>(l.size()-1)) {h=(h%l.size());}   
+         l.remove(h);h--;
+         ost--;
+       }
+     System.out.println("Безопасная позиция "+l.get(0));
+     
+     }
+ 
+}
