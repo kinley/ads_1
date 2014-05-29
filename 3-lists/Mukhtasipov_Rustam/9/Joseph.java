0ca5046aea7e7f8794998c1ca84d137b4d0fd3a8
Far4Mac <far4mac@gmail.com>
2014-05-29 13:30:05 +0400

    commit

--- 3-lists/Mukhtasipov_Rustam/9/Joseph.java
+++ 3-lists/Mukhtasipov_Rustam/9/Joseph.java
@@ -6,21 +6,24 @@ public class Joseph {
      System.out.println("Введите количество:");
      int n=sc.nextInt();sc.nextLine();           
      System.out.println("Введите номер исключаемого:");
-     int m=sc.nextInt();sc.nextLine();           
+     int m=sc.nextInt();sc.nextLine();
+     	if (m!=1){
      List<Integer> l=new ArrayList<Integer>();
-     for (int i=1;i<=n;i++)
-         l.add(i);                              
+     	for (int i=1;i<=n;i++)
+     		l.add(i);                              
      int ost=n;
      int h=-1;
      while(ost>1)
        {
-         h+=m;
-         if (h>(l.size()-1)) {h=(h%l.size());}   
+         h+=m;  
+         if (h>(l.size()-1)) {h=(h%l.size());} 
          l.remove(h);h--;
          ost--;
        }
      System.out.println("Безопасная позиция "+l.get(0));
-     
+     }
+     else
+    	 System.out.println("Нет безопасных позиций");
      }
  
 }
