bba7e109002636dce55ad7d86efa385df9350ece
akkaynt13 <akkayht13@gmail.com>
2014-05-29 19:26:56 +0400

    чутка переделал, может оно так должно быть...

--- 1-classes/Yamalov_Vladislav/DlinnoeChislo.java
+++ 1-classes/Yamalov_Vladislav/DlinnoeChislo.java
@@ -4,51 +4,47 @@ import java.util.Scanner;
 
 public class DlinnoeChislo {
 	public static int c;
-	public static long b;
+	public static int b;
 	public static long d;
 	public static void main(String[] args){
 		 Scanner sc=new Scanner(System.in);
 		 c=sc.nextInt();
-		 b=sc.nextLong();
+		 b=sc.nextInt();
 		
 	}
-	public static long vichislit(int c, long b){
+	public static long vichislit(int c, int b){
 		double a=c*Math.pow(2, 15)+b;
-		long l=(new Double(a)).longValue();
-		System.out.println(l);
-		return l;
+		d=(new Double(a)).longValue();
+		System.out.println(d);
+		return d;
 	}
 	public void proizvedenie(){
-		long t=d;
 		BigInteger f,l,e;
 		l=BigInteger.valueOf(d);
-		e=BigInteger.valueOf(t);
+		e=BigInteger.valueOf(c);
 		f=l.multiply(e);
 		System.out.println(f.toString());
 	}
 	public void summa(){
-		long t=d;
 		BigInteger f,l,e;
 		l=BigInteger.valueOf(d);
-		e=BigInteger.valueOf(t);
+		e=BigInteger.valueOf(c);
 		f=l.add(e);
 		System.out.println(f.toString());
 	}
 	public void raznost(){
-		long t=d;
 		BigInteger f,l,e;
 		l=BigInteger.valueOf(d);
-		e=BigInteger.valueOf(t);
+		e=BigInteger.valueOf(c);
 		e=e.negate();
 		f=l.add(e);
 		System.out.println(f.toString());
 		
 	}
 	public void sravn(){
-		long t=d;
 		BigInteger l,e;
 		l=BigInteger.valueOf(d);
-		e=BigInteger.valueOf(t);
+		e=BigInteger.valueOf(c);
 		int f=l.compareTo(e);
 		System.out.println(f);
 	}
