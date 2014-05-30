6c16dcc11c21f0204d03b3a9d35495c5cd0d46a5
valeevbulat <cirkach95@gmail.com>
2014-05-30 13:48:37 +0400

    class

--- 1-classes/Valeev_Bulat/parall.java
+++ 1-classes/Valeev_Bulat/parall.java
@@ -1,61 +1,55 @@
 package class_18;
 
 
-
 class parallParam{//Описываем класс параллелограмма
 	
 	// Свойства класса
-	public double d1;// Большая диагональ
-	public double d2;// Малая диагональ
-	public double ugol;// Меньший угол между диагоналями
-	public double x;// Ширина
-	public double y;// Длина
-	public double u1;// Острый угол парал.
-	public double u2;// Тупой угол парал.
-	
+	private double d1;// Большая диагональ
+	private double d2;// Малая диагональ
+	private double ugol;// Меньший угол между диагоналями
 	// Методы класса
 	// Создать парал.
-	public parallParam(double a, double b, double c){
-		d1 = a;
-		d2 = b;
-		ugol = c;
-		//Выводим на экран значения
-		System.out.println(d1);
-		System.out.println(d2);
-		System.out.println(ugol);
+	public parallParam(double a, double b, double c){//Конструктор
+		this.d1 = a;
+		this.d2 = b;
+		this.ugol = c;
 	}
-	
+
 	// Длины сторон
-	public void lengthSides(){
-		x = Math.sqrt(Math.pow( d1, 2 ) + Math.pow( d2, 2 ) - 2*d1*d2*Math.cos(ugol))/2;
-		y = Math.sqrt(Math.pow( d1, 2 ) + Math.pow( d2, 2 ) + 2*d1*d2*Math.cos(ugol))/2;
-		//Выводим на экран значения
-		System.out.println(x);
-		System.out.println(y);
+	public double lengthSidesX(){
+		double x = Math.sqrt(Math.pow( this.d1, 2 ) + Math.pow( this.d2, 2 ) - 2*this.d1*this.d2*Math.cos(this.ugol))/2;
+		return x;
 	}
-	// Углы
-	public void angles(){
-		u1 = Math.acos((Math.pow(x, 2)+Math.pow(y, 2)-Math.pow(d2, 2))/(2*x*y));
+	// Длины сторон
+	public double lengthSidesY(){
+		double y = Math.sqrt(Math.pow( this.d1, 2 ) + Math.pow( this.d2, 2 ) + 2*this.d1*this.d2*Math.cos(this.ugol))/2;
+		return y;
+	}
+	// Углы 1
+	public double angles1(){
+		double u1 = Math.acos((Math.pow(lengthSidesX(), 2)+Math.pow(lengthSidesY(), 2)-Math.pow(this.d2, 2))/(2*lengthSidesX()*lengthSidesY()));
 		u1 = 180 - (180 / Math.PI) * u1;
-		u2 = 180 - u1;
-		//Выводим на экран значения
-		System.out.println(u1);
-		System.out.println(u2);
+		return u1;
 	}
-	// "Сжатие вытягивание"
-	public void xysize(double k1, double k2){
-		d1 = d1*k1;
-		d2 = d2*k2;
-		//Выводим на экран значения
-		System.out.println(d1);
-		System.out.println(d2);
+	// Углы 2
+	public double angles2(){
+		double u2 = Math.acos((Math.pow(lengthSidesX(), 2)+Math.pow(lengthSidesY(), 2)-Math.pow(this.d2, 2))/(2*lengthSidesX()*lengthSidesY()));
+		u2 = (180 / Math.PI) * u2;
+		return u2;
 	}
 	// Area
-	public void area(){
-		double ar;
-		ar = x*y* Math.sin(u1);
-		//Выводим на экран значения
-		System.out.println(ar);
+	public double area(){
+		double ar = lengthSidesX()*lengthSidesY()* Math.sin(angles1());
+		return ar;
+		
+	}
+
+	// "Сжатие вытягивание"
+	public parallParam xysize(parallParam P, double k1, double k2){
+		P.d1 = P.d1*k1;
+		P.d2 = P.d2*k2;
+		P.ugol = (180/Math.PI) * ((Math.pow(this.d1, 2)+Math.pow(this.d2, 2)-4*Math.pow(lengthSidesX(), 2))/(2*this.d1*this.d2));
+		return P;
 	}
 }
 
@@ -64,19 +58,17 @@ public class parall {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		parallParam A= new parallParam(2,8,30);
+		
 		System.out.println("-----------Длины------------");
-		A.lengthSides();
+		System.out.print(A.lengthSidesX()); 
 		System.out.println("-----------Углы------------");
-		A.angles();
+		A.angles1();
 		System.out.println("-----------Площадь------------");
 		A.area();
 		System.out.println("-----------Растяжение------------");
-		A.xysize(2,2);
+		A.xysize(A,2,2);
 		System.out.println("-----------Новые значения сторон------------");
-		A.lengthSides();
-		System.out.println("-----------Новая площадь------------");
-		A.area();
-		System.out.println("-----------------------");
+		A.lengthSidesX();
 	}
 
 }
